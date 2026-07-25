318c36a9715e18dd9b80258dbddc39e0f581a7bf
mikiyasdessu-coder <mikiyasdessu@gmail.com>
2026-07-25 15:03:12 +0300

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+# Logging ማስተካከያ
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+# /start ሲባል የሚሰጠው ምላሽ
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("ሰላም! ቦቱ በትክክል እየሰራ ነው! 🚀")
+
+if __name__ == '__main__':
+    # የቴሌግራም ቦት Token ህን እዚህ ቦታ ላይ በግልጽ አስገባ (ወይም Environment Variable ተጠቀም)
+    TOKEN = "8591437134:AAGSM3A0RoWss0t7Gr0Jt2xgvmE_ZX-fD5E "
+    
+    app = ApplicationBuilder().token(TOKEN).build()
+    
+    # Command Handlers
+    app.add_handler(CommandHandler("start", start))
+    
+    print("Bot is running...")
+    app.run_polling()
